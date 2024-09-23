6e85eea4875fc60a1850fb71359cf3a60a762d49
Trung Kien <trungkienlx02@gmail.com>
2024-09-23 08:45:29 +0700

    [update] student_management
    update view contacts

--- odoo-16.0/student_management/__manifest__.py
+++ odoo-16.0/student_management/__manifest__.py
@@ -8,10 +8,10 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/views.xml',
         'views/menus.xml',
         'views/res_partner_views.xml',
-        'views/student_views.xml',
-        'views/views.xml',
+        # 'views/student_views.xml',
     ],
     'installable': True,
     'application': True,
